139cfa38a30dc0481b29de97ed1fd37c4fc7efe2
曹胜楠 <caoshengnan@caoshengnandeMacBook-Air.local>
2018-10-25 21:35:50 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3030;
+// const formidable = require('formidable');
+// const fileUpload = require('express-fileupload');
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -26,19 +30,82 @@ app.use((req, res, next) => {
     });
     next();
 });
+// app.use(fileUpload());
+
+
+
+
+
+
 app.set('view engine', 'hbs');
 
 app.get('/',(req, res) => {
     // res.send('<h1>Hello Express</h1>');
     res.render('home.hbs', {
-        pageTitle: 'Home Page',
-        welcomeMessage: 'Go go!'
+        pageTitle: 'Public Peer Review Platform',
+        // welcomeMessage: 'Go go!'
     });
 });
 
+
+
+
+
+// app.get('/upload', (req, res) => {
+//     res.render('upload.hbs', {
+//         pageTitle: 'Upload Page'
+//     });
+// });
+
+
+
+app.get('/upload', (req, res) => {
+    res.render('upload.hbs', {
+        pageTitle: 'Upload Page'
+    });
+});
+
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
+        pageTitle: 'About Page'
+    });
+});
+app.get('/physics', (req, res) => {
+    res.render('physics.hbs', {
+        pageTitle: 'Physics Page'
+    });
+});
+app.get('/math', (req, res) => {
+    res.render('math.hbs', {
+        pageTitle: 'Math Page'
+    });
+});
+app.get('/cs', (req, res) => {
+    res.render('cs.hbs', {
+        pageTitle: 'CS Page'
+    });
+});
+app.get('/econ', (req, res) => {
+    res.render('econ.hbs', {
+        pageTitle: 'Economics Page'
+    });
+});
+
+app.get('/material', (req, res) => {
+    res.render('material.hbs', {
+        pageTitle: 'Material Page'
+    });
+});
+
+app.get('/donation', (req, res) => {
+    res.render('donation.hbs', {
+        pageTitle: 'Donation Page'
+    });
+});
+app.get('/help', (req, res) => {
+    res.render('help.hbs', {
+        pageTitle: 'Help Page'
     });
 });
 app.get('/bad', (req, res) => {
@@ -46,4 +113,4 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Error handling request'
     });
 });
-app.listen(3030);
+app.listen(port);
